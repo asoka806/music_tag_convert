22b7662e0e744127d62a34a8d4a5b236cff33715
Cao Qi <qicao@sysnew.com>
2023-11-10 09:42:30 +0800

    优化错误处理

--- file.py
+++ file.py
@@ -6,22 +6,34 @@ def rename(fileName, track_no, artist, title, discnumber, totaldiscs):
     (path, oldName) = os.path.split(fileName)
     (file, ext) = os.path.splitext(oldName)
 
-    newfile = track_no + ' - ' + artist + ' - ' + title
+    if len(title) > 0:
+        if len(artist) > 0:
+            if len(track_no) > 0:
+                newfile = track_no + ' - ' + artist + ' - ' + title
+            else:
+                newfile = artist + ' - ' + title
+        else:
+            newfile = title
+    else:
+        print(f'{fileName} no title skip!')
+        return
+
     if len(totaldiscs) > 0 and int(totaldiscs) > 1:
         newfile = discnumber + ' - ' + newfile
+
     newpath = os.path.join(path, newfile + ext)
     try:
         os.rename(fileName, newpath)
     except OSError as e:
-        print(f'rename error: {e.errno}, Error text: {e.strerror}')
+        print(f'rename {fileName} error: {e.errno}, Error text: {e.strerror}')
     # 寻找同级目录下的lrc歌词文件并重命名
     (tmp_path, ext) = os.path.splitext(fileName)
     lrc_file = tmp_path + '.lrc'
-    if os.path.exists():
+    if os.path.exists(lrc_file):
         new_lrc_path = os.path.join(path, newfile + '.lrc')
         try:
             os.rename(lrc_file, new_lrc_path)
         except OSError as e:
-            print(f'rename lrc error: {e.errno}, Error text: {e.strerror}')
+            print(f'rename {fileName} lrc error: {e.errno}, Error text: {e.strerror}')
 
     print(newfile + ext + ' done')
